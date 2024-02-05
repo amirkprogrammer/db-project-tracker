ddad0aad1efdfce9f1903a25b5a39f506505ced4
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-02-05 12:02:21 -0800

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Amir Kabiri on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
